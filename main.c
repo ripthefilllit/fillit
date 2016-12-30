023f7d6173397d92d41012ed010c3251de1893ac
Benjamin ZMUDA <bzmuda@e2r5p13.42.fr>
2016-12-30 15:29:01 +0100

    esquimaux

--- main.c
+++ main.c
@@ -6,31 +6,9 @@
 /*   By: bzmuda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/30 10:06:18 by bzmuda            #+#    #+#             */
-/*   Updated: 2016/12/30 11:01:06 by bzmuda           ###   ########.fr       */
+/*   Updated: 2016/12/30 13:39:12 by bzmuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#define BUF_SIZE 546 // nb max de caracteres + \n pour 26 tetriminos
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-
-int check_file(char **tab);
-int verif_body(char *s);
-int get_w(char *s);
-int get_h(char *s);
-int another_check(char *s);
-int cpt_tetrimino(char **tab);
-int detective_alone(char *s);
-int collect_char(char *s);
-int verif_line(char *s);
-char *file_to_array(char *file);
-char **split_array(char *array);
 
 int		main(int argc, char **argv)
 {
