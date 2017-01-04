efb5b98e867b4af4f50ff9e734e12bbacbffc74c
Brice Waegeneire <brice.wge@gmail.com>
2017-01-04 12:40:36 +0100

    fix tests and add comment

--- src/main.c
+++ src/main.c
@@ -6,11 +6,24 @@
 /*   By: bzmuda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/30 10:06:18 by bzmuda            #+#    #+#             */
-/*   Updated: 2017/01/03 15:05:56 by bwaegene         ###   ########.fr       */
+/*   Updated: 2017/01/04 12:36:37 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+void debug_array(char **tblstr)
+{
+	size_t i;
+
+	i = 0;
+	while (tblstr[i])
+	{
+		ft_putstr(tblstr[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
 
 int		main(int argc, char **argv)
 {
@@ -22,14 +35,17 @@ int		main(int argc, char **argv)
 		ft_putstr_fd("usage: fillit file\n", 2);
 		return (1);
 	}
-	lol = file_to_array(argv[1]);
+	lol = file_to_str(argv[1]);
 	if (!lol || !*lol)
 	{
 		ft_putstr_fd("error\n", 2);
 		return (1);
 	}
-	zen = split_array(lol);
-	mark_tetriminos(zen);
+	zen = str_to_tblstr(lol);
+	/* ft_putstr(zen[4]); */
+	debug_array(zen);
+	//mark_tetriminos(zen);
+	//ft_putstr(empty_square(3));
 	/* if (!check_file(zen)) */
 	/* { */
 	/* 	ft_putstr_fd("error\n", 2); */
@@ -37,6 +53,6 @@ int		main(int argc, char **argv)
 	/* } */
 	// Stocker every tetriminos dans une linked list ?
 	// Resolution
-	 
+	return (0);
 }
 // Ajouter la lib putchar strlen putstr putstrfd needed
--- src/mark_tetrimino.c
+++ src/mark_tetrimino.c
@@ -6,11 +6,16 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/03 13:47:57 by bwaegene          #+#    #+#             */
-/*   Updated: 2017/01/03 15:06:20 by bwaegene         ###   ########.fr       */
+/*   Updated: 2017/01/04 09:50:54 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+/*
+** Identify the tetriminos by assiging a unique upper letter to each one, on an
+** alphabetical order.
+*/
 
 void	mark_tetriminos(char **tetriminos)
 {
